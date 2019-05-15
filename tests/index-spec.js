48bf1505c9d24984f10eaedc0009e84bb32f5d38
DMa <davidweibinma@Weibins-MacBook-Pro-2.local>
2019-05-14 20:59:44 -0400

    added index-spec.js

---
+++ tests/index-spec.js
@@ -0,0 +1,17 @@
+// import { describe } from "mocha";
+
+import { describe } from 'mocha';
+
+const chai = require('chai');
+
+const {expect} = chai;
+const server = require('../server');
+
+console.log('test file, server = \n', server);
+
+describe('test', () => {
+  it('this simple test should return a string', () => {
+    expect('Test CI with Travis').to.equal('Test CI with Travis');
+  });
+  process.exit();
+});
